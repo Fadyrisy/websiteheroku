3f7970206a91669f0915e83eebe6a0d80a627339
Fadyrisy <102226255+Fadyrisy@users.noreply.github.com>
2022-03-26 01:41:22 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,2 @@
+bot_urls = ['https://the-one-monitor.hexayanbusiness.repl.co/', 'https://www.hexayanbusiness.repl.co/', 'https://the-one-monitor.nakiboy-kunkun.repl.co/', 'https://discord-musicbot.hexayanbusiness.repl.co/']
+timeout = 10
